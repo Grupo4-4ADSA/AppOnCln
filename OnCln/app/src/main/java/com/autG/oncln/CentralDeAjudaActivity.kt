726fe2c8f94ec4f12b5029909f8d8d8d127f554f
Vitoria da Silva <vitoriasilva98@hotmail.com>
2022-09-30 15:20:22 -0300

    Desenvolvimento de layout (Tela central de ajuda)

---
+++ OnCln/app/src/main/java/com/autG/oncln/CentralDeAjudaActivity.kt
@@ -0,0 +1,11 @@
+package com.autG.oncln
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CentralDeAjudaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_central_de_ajuda)
+    }
+}
