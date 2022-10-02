d9462bb69fbc535f9c195490f631505f143fcf78
Vitoria da Silva <vitoriasilva98@hotmail.com>
2022-10-01 23:10:26 -0300

    Desenvolvimento de layout (Tela de configurações)

---
+++ OnCln/app/src/main/java/com/autG/oncln/ConfiguracoesActivity.kt
@@ -0,0 +1,14 @@
+package com.autG.oncln
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConfiguracoesActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_configuracoes)
+    }
+
+}
