234481d47183e4cbac2cfcae50dd882106660f53
Mateuzx <araujonascimentosp@gmail.com>
2022-10-09 00:57:48 -0300

    removendo coisas do commit

--- OnCln/app/src/main/java/com/autG/oncln/CadastrarEquipamento.kt
+++ OnCln/app/src/main/java/com/autG/oncln/CadastrarEquipamento.kt
@@ -1,11 +1,12 @@
-package com.autG.oncln
+package com.example.telasoncln
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.autG.oncln.R
 
-class MainActivity : AppCompatActivity() {
+class CadastrarEquipamento : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_cadastrar_equipamento)
     }
 }
