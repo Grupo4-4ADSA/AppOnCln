1f421c2616edfdf687de383e2b470e7507063cc1
Mateuzx <araujonascimentosp@gmail.com>
2022-10-12 15:22:45 -0300

    Enviando oq faltava

---
+++ OnCln/app/src/main/java/com/autG/oncln/Login.kt
@@ -0,0 +1,12 @@
+package com.example.telasoncln
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.autG.oncln.R
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
