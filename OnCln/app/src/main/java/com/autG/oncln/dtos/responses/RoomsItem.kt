b5401ef2321c3edc11e9508de0802af7c43387b1
elizeu12 <elizeu.souza@bandtec.com.br>
2022-10-26 19:54:07 -0300

    alterações item

--- OnCln/app/src/main/java/com/autG/oncln/dtos/responses/RoomsItem.kt
+++ OnCln/app/src/main/java/com/autG/oncln/dtos/responses/RoomsItem.kt
@@ -3,5 +3,6 @@ package com.autG.oncln.dtos.responses
 data class RoomsItem(
     val floor: Int,
     val idRoom: Int,
-    val name: String
+    val name: String,
+    private val onClick: (msg: String) -> Unit
 )
